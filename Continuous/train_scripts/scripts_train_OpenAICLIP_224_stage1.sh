761a8404724a0cb6ec85a03f72eb0807e881174a
Bowa529 <bowa010529@gmail.com>
2025-12-22 01:13:16 +0800

    Add mixed dilation schedule for temporal conditioning

--- Continuous/train_scripts/scripts_train_OpenAICLIP_224_stage1.sh
+++ Continuous/train_scripts/scripts_train_OpenAICLIP_224_stage1.sh
@@ -1,4 +1,5 @@
 export AE="/data/zby/GenHancer/Continuous/src/flux/FLUX.1-dev/ae.safetensors"
 
 # accelerate launch --config_file "train_configs/accelerate_config.yaml" train_OpenAICLIP_nextpredic_stage1.py --config "train_configs/test_OpenAICLIP_224_stage1.yaml"
-accelerate launch --config_file "train_configs/accelerate_config.yaml" train_OpenAICLIP_sliding_windows_nextpredic_stage1.py --config "train_configs/test_OpenAICLIP_224_stage1_sliding_window.yaml"
+# accelerate launch --config_file "train_configs/accelerate_config.yaml" train_OpenAICLIP_sliding_windows_nextpredic_stage1.py --config "train_configs/test_OpenAICLIP_224_stage1_sliding_window.yaml"
+accelerate launch --config_file "train_configs/accelerate_config.yaml" train_OpenAICLIP_indefinite_length_sliding_windows_nextpredic_stage1.py --config "train_configs/test_OpenAICLIP_224_stage1_indefinite_length_sliding_window.yaml"
--- Continuous/train_scripts/scripts_train_OpenAICLIP_224_stage2_all.sh
+++ Continuous/train_scripts/scripts_train_OpenAICLIP_224_stage2_all.sh
@@ -1,4 +1,5 @@
 export AE="/data/zby/GenHancer/Continuous/src/flux/FLUX.1-dev/ae.safetensors"
 
 # accelerate launch --config_file "train_configs/accelerate_config.yaml" train_OpenAICLIP_use2frames_nextpredic_stage2_all.py --config "train_configs/test_OpenAICLIP_224_stage2_all.yaml"
-accelerate launch --config_file "train_configs/accelerate_config.yaml" train_OpenAICLIP_sliding_windows_nextpredic_stage2_all.py --config "train_configs/test_OpenAICLIP_224_stage2_all_sliding_window.yaml"
+# accelerate launch --config_file "train_configs/accelerate_config.yaml" train_OpenAICLIP_sliding_windows_nextpredic_stage2_all.py --config "train_configs/test_OpenAICLIP_224_stage2_all_sliding_window.yaml"
+accelerate launch --config_file "train_configs/accelerate_config.yaml" train_OpenAICLIP_indefinite_length_sliding_windows_nextpredic_stage2_all.py --config "train_configs/test_OpenAICLIP_224_stage2_all_indefinite_length_sliding_window.yaml"
